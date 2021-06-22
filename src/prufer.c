7a4368477b74d37ca66d9e6429b722b223e7d6e3
quanganhquanganh <59202082+quanganhquanganh@users.noreply.github.com>
2021-06-22 21:57:12 +0700

    Prufer Update 2.

--- src/prufer.c
+++ src/prufer.c
@@ -16,55 +16,6 @@ int pruferInputGraph(FILE* f, Graph g) {
     return so_dinh;
 }
 
-void prufer_2(Graph g, int start, int remainingVertices, int* code, int* length) {
-    int min = 1000;
-    int temp[100];
-    int n, visited[1000] = {0};
-    int output[100], u, v;
-    *length = 0;
-    Dllist node, queue;
-    queue = new_dllist();
-    dll_append(queue, new_jval_i(start));
-    while (!dll_empty(queue))
-    {
-        node = dll_first(queue);
-        u = jval_i(node->val);
-        dll_delete_node(node);
-        if (!visited[u])
-        {
-            if (outdegree(g, u, temp) == 1)
-            {
-                if (u < min && u != start)
-                    min = u;
-            }
-            visited[u] = 1;
-            n = outdegree(g, u, output);
-            for (int i = 0; i < n; i++)
-            {
-                v = output[i];
-                if (!visited[v])
-                    dll_append(queue, new_jval_i(v));
-            }
-        }
-    }
-
-    if (remainingVertices == 2)
-    {
-        //printf("\n");
-        return;
-    }
-    else
-    {
-        outdegree(g, min, output);
-        removeEdge(g, min, output[0]);
-        //removeEdge(g, output[0], min);
-        code[(*length)++] = output[0];
-        //printf("%4d", output[0]);
-        remainingVertices--;
-        prufer_2(g, start, remainingVertices, code, length);
-    }
-}
-
 void prufer(Graph g, int output[], int* length) {
     *length = 0;
     /*
