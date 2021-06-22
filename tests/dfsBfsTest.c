0a7ea836e46043744bc20c10727fefbc461e4d17
quanganhquanganh <59202082+quanganhquanganh@users.noreply.github.com>
2021-06-22 15:44:31 +0700

    Traversal Tests Update.

---
+++ tests/dfsBfsTest.c
@@ -0,0 +1,27 @@
+#include "../src/graph.h"
+#include <stdio.h>
+
+int arr[100], i = 0;
+void addToArr(int m) {
+    arr[i++] = m;
+}
+
+int main() {
+    Graph g = createGraph();
+    addEdge(g, 0, 1, 1);
+    addEdge(g, 1, 2, 1);
+    addEdge(g, 1, 3, 1);
+    addEdge(g, 2, 3, 1);
+    addEdge(g, 2, 4, 1);
+    addEdge(g, 4, 5, 1);
+    addEdge(g, 1, 0, 1);
+    addEdge(g, 2, 1, 1);
+    addEdge(g, 3, 1, 1);
+    addEdge(g, 3, 2, 1);
+    addEdge(g, 4, 2, 1);
+    addEdge(g, 5, 4, 1);
+
+    BFS(g, 0, -1, addToArr);
+    for(int j = 0; j < i; ++j)
+        printf("%4d", arr[j]);
+}
